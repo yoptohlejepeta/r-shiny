2edb6df3a1854ca6a7d36dc3e180224c2e650411
yoptohlejepeta <peta.kotlan@seznam.cz>
2024-03-08 23:15:33 +0100

    basic project structure

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# r-shiny
+# R shiny
+
+![R](https://img.shields.io/badge/r-%23276DC3.svg?style=for-the-badge&logo=r&logoColor=white)
+
 Learning shiny apps
+
+
+[renv](https://ecorepsci.github.io/reproducible-science/renv.html)
+
+## Run app
+
+run the main.r file
---
+++ app/ui.r
@@ -0,0 +1,18 @@
+library(shiny)
+source("server.r")
+
+ui <- fluidPage(
+  titlePanel("Old Faithful Geyser Data"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("bins",
+                  "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+    ),
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+)
